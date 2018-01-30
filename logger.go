34db6e3ec8d3303215cb7693082dda09fda9da41
Patrik Wallström <patrik@brickchain.com>
2018-01-30 17:12:09 +0100

    added better documentation

--- logger.go
+++ logger.go
@@ -1,5 +1,25 @@
 /*
-Logger for Brickchain software
+The package Logger is a wrapper for the [Logrus](https://github.com/Sirupsen/logrus) logger package.
+
+Logger is used by most Brickchain software to enable context based logging with details needed by each component.
+
+
+Installation
+
+	# How to install logger
+	$ go get github.com/Brickchain/go-logger.v1
+
+
+Example
+
+	logger.SetOutput(os.Stdout)
+	logger.SetFormatter("text")
+	logger.SetLevel("debug")
+	logger.AddContext("service", path.Base(os.Args[0]))
+	logger.AddContext("version", constant.Version)
+
+	logger.Infof("Running version %s", version)
+	logger.Error(err)
 */
 package logger
 
@@ -20,10 +40,12 @@ var (
 	mu        *sync.Mutex
 )
 
+// Entry contains a Logrus entry
 type Entry struct {
 	entry *logrus.Entry
 }
 
+// Fields is a map of fields used by the logger
 type Fields map[string]interface{}
 
 func init() {
@@ -36,16 +58,19 @@ func init() {
 
 }
 
+// GetLogger returns the context logger
 func GetLogger() *logrus.Entry {
 	return ctxlogger
 }
 
+// AddContext adds another context to the logger
 func AddContext(key string, value interface{}) {
 	mu.Lock()
 	defer mu.Unlock()
 	ctxlogger = ctxlogger.WithField(key, value)
 }
 
+// SetFormatter sets the formatter to be used, either "json" or "text"
 func SetFormatter(formatter string) {
 	var _formatter logrus.Formatter
 	switch formatter {
@@ -61,6 +86,7 @@ func SetFormatter(formatter string) {
 	ctxlogger = logrus.WithFields(data)
 }
 
+// SetOutput sets the io.Writer to use
 func SetOutput(out io.Writer) {
 	mu.Lock()
 	defer mu.Unlock()
@@ -69,6 +95,7 @@ func SetOutput(out io.Writer) {
 	ctxlogger = logrus.WithFields(data)
 }
 
+// SetLevel sets the lowest level to log
 func SetLevel(level string) {
 	_level, err := logrus.ParseLevel(level)
 	if err != nil {
@@ -81,14 +108,17 @@ func SetLevel(level string) {
 	ctxlogger = logrus.WithFields(data)
 }
 
+// GetLoglevel returns the current log level
 func GetLoglevel() string {
 	return logrus.GetLevel().String()
 }
 
+// WithField adds another field to the Logrus entry context
 func WithField(key string, value interface{}) *logrus.Entry {
 	return ctxlogger.WithField(key, value)
 }
 
+// WithFields adds more Fields to the Logrus entry context
 func WithFields(fields Fields) *logrus.Entry {
 	_fields := logrus.Fields{}
 	for k, v := range fields {
@@ -97,47 +127,52 @@ func WithFields(fields Fields) *logrus.Entry {
 	return ctxlogger.WithFields(_fields)
 }
 
-// Wrapper for Logrus Debug()
+// Debug is the wrapper for Logrus Debug()
 func Debug(args ...interface{}) {
 	loggerWithCaller().Debug(args...)
 }
 
-// Wrapper for Logrus Info()
+// Info is the wrapper for Logrus Info()
 func Info(args ...interface{}) {
 	loggerWithCaller().Info(args...)
 }
 
-// Wrapper for Logrus Warn()
+// Warn is the wrapper for Logrus Warn()
 func Warn(args ...interface{}) {
 	loggerWithCaller().Warn(args...)
 }
 
-// Wrapper for Logrus Error()
+// Error is the wrapper for Logrus Error()
 func Error(args ...interface{}) {
 	loggerWithCaller().Error(args...)
 }
 
-// Wrapper for Logrus Fatal()
+// Fatal is the wrapper for Logrus Fatal()
 func Fatal(args ...interface{}) {
 	loggerWithCaller().Fatal(args...)
 }
 
+// Fatalf is the wrapper for Logris Fatalf()
 func Fatalf(format string, args ...interface{}) {
 	loggerWithCaller().Fatalf(format, args...)
 }
 
+// Errorf is the wrapper for Logrus Errorf()
 func Errorf(format string, args ...interface{}) {
 	loggerWithCaller().Errorf(format, args...)
 }
 
+// Infof is the wrapper for Logrus Infof()
 func Infof(format string, args ...interface{}) {
 	loggerWithCaller().Infof(format, args...)
 }
 
+// Warningf is the wrapper for Logrus Warningf()
 func Warningf(format string, args ...interface{}) {
 	loggerWithCaller().Warningf(format, args...)
 }
 
+// Debugf is the wrapper for Logrus Debugf()
 func Debugf(format string, args ...interface{}) {
 	loggerWithCaller().Debugf(format, args...)
 }
@@ -151,40 +186,48 @@ func loggerWithCaller() *logrus.Entry {
 	return ctxlogger.WithFields(fields)
 }
 
+// GetWriter returns the current io.Writer (with level "info")
 func GetWriter() io.Writer {
 	return &LogWriter{
 		level: "info",
 	}
 }
 
+// GetWriterWithLevel returns the current io.Writer with the level given as argument
 func GetWriterWithLevel(level string) io.Writer {
 	return &LogWriter{
 		level: level,
 	}
 }
 
+// LogWriter contains the log level
 type LogWriter struct {
 	level string
 }
 
+// Write: TODO docs
 func (l *LogWriter) Write(p []byte) (n int, err error) {
 	switch l.level {
 	case "debug":
 		Debug(string(p))
 	case "info":
 		Info(string(p))
+	case "warn", "warning":
+		Warn(string(p))
 	case "error":
 		Error(string(p))
 	}
 	return len(p), nil
 }
 
+// GetStandardLogger returns a standard "log" logger
 func GetStandardLogger(level string) *log.Logger {
 	writer := GetWriterWithLevel(level)
 
 	return log.New(writer, "", 0)
 }
 
+// ForContext TODO docs
 func ForContext(ctx context.Context) *Entry {
 	reqID, _ := ctx.Value(0).(string)
 	return &Entry{
@@ -192,11 +235,13 @@ func ForContext(ctx context.Context) *Entry {
 	}
 }
 
+// WithField adds another field to the Logrus entry context
 func (e *Entry) WithField(key string, value interface{}) *Entry {
 	e.entry = e.entry.WithField(key, value)
 	return e
 }
 
+// WithFields adds more Fields to the Logrus entry context
 func (e *Entry) WithFields(fields Fields) *Entry {
 	_fields := logrus.Fields{}
 	for k, v := range fields {
@@ -206,43 +251,47 @@ func (e *Entry) WithFields(fields Fields) *Entry {
 	return e
 }
 
-// Wrapper for Logrus Debug()
+// Debug is the wrapper for Logrus Debug()
 func (e *Entry) Debug(args ...interface{}) {
 	e.loggerWithCaller().entry.Debug(args...)
 }
 
-// Wrapper for Logrus Info()
+// Info is the wrapper for Logrus Info()
 func (e *Entry) Info(args ...interface{}) {
 	e.loggerWithCaller().entry.Info(args...)
 }
 
-// Wrapper for Logrus Warn()
+// Warn is the wrapper for Logrus Warn()
 func (e *Entry) Warn(args ...interface{}) {
 	e.loggerWithCaller().entry.Warn(args...)
 }
 
-// Wrapper for Logrus Error()
+// Error is the wrapper for Logrus Error()
 func (e *Entry) Error(args ...interface{}) {
 	e.loggerWithCaller().entry.Error(args...)
 }
 
-// Wrapper for Logrus Fatal()
+// Fatal is the wrapper for Logrus Fatal()
 func (e *Entry) Fatal(args ...interface{}) {
 	e.loggerWithCaller().entry.Fatal(args...)
 }
 
+// Errorf is the wrapper for Logrus Errorf()
 func (e *Entry) Errorf(format string, args ...interface{}) {
 	e.loggerWithCaller().entry.Errorf(format, args...)
 }
 
+// Infof is the wrapper for Logrus Infof()
 func (e *Entry) Infof(format string, args ...interface{}) {
 	e.loggerWithCaller().entry.Infof(format, args...)
 }
 
+// Warningf is the wrapper for Logrus Warninf()
 func (e *Entry) Warningf(format string, args ...interface{}) {
 	e.loggerWithCaller().entry.Warningf(format, args...)
 }
 
+// Debugf is the wrapper for Logrus Debugf()
 func (e *Entry) Debugf(format string, args ...interface{}) {
 	e.loggerWithCaller().entry.Debugf(format, args...)
 }
